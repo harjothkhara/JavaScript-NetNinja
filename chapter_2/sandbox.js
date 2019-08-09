44f8752661c8596f95ceab6659e588a230fff023
harjoth <harjoth.khara@gmail.com>
2019-08-09 15:11:51 -0700

    let variable keyword allows us to overide later on since its saved in memory. create a variable but we don't want the value to change - use the const keyword. if you want to change the value of a variable later on use let, if you don't want to change the value of a variable then use const. both let and const are new to js. Var is the older way to create variables. we use camel case(lools like a hump on a camels back) for multi-word variable names e.g myAge...variable can contain only letters, numbers, underscores or dollar signs but they can't start with a number. finally there are some reserved keywords that we can't use for variable names such as let and const which are variable keywords not used for variable names.

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,2 +1,17 @@
-console.log(1);
-console.log(2);
+// use the let keyword if you want to change the variable value later.
+let age = 25;
+let year = 2019;
+
+// log things to console.
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+// use a const if we don't want the value assigned to it to be overriden.
+const points = 100;
+// points = 50; get an error with this as you can only assign a const variable once.
+console.log(points);
+//older ways to create variables
+var score = 75;
+console.log(75);
