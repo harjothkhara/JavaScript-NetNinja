2b46a5bc2583665bbb4b126c524d1cca304b457a
harjoth <harjoth.khara@gmail.com>
2019-08-10 11:37:17 -0700

    type conversion... Number(), String(), Boolean() typeof()... positive and negative numbers are considered "truthy" values and 0 is "falsey". strings of any type are all "truthy", an empty string is "falsey"

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,14 +1,28 @@
-let age = 25;
+// type conversion - turning one data type into another data type
 
-// loose comparison (different types can still be equal); types= number, string...
+//let score = '100';
+//console.log(score + 1); // 1001 its concatenating a string and a number instead od adding numbers
 
-//console.log(age == 25); // true
-//console.log(age == '25'); // true
-//console.log(age != 25); // false
-//console.log(age != '25'); // false
+//let score = '100';
+//score variable value is converted from a string into a number
+//score = Number(score);
+//console.log(score + 1);  // 101
 
-// strict comparison (different types cannot be equal);
-console.log(age === 25); // true
-console.log(age === '25'); // false - no type conversion going on now behind the scenes
-console.log(age !== 25);  // false
-console.log(age !== '25');  // true - age is not equal to the string 25, its equal to the number 25
+//let score = '100';
+// checking for type
+//console.log(typeof score); // string
+
+//let score = '100';
+// doesn't make sense to turn this into a number but lets try it!
+//let result = Number('hello');
+//console.log(result); //NaN
+
+//let score = '100';
+// turing from a numbers to a string
+//let result = Boolean(50) // positive and negative numbers are considered a "truthy" value in type conversion, but 0 is false.
+//console.log(result, typeof result); // true "boolean"
+
+let score = '100';
+// turing from a numbers to a string
+let result = Boolean('0') // strings of any length are all "truthy", an empty string would be "falsy"
+console.log(result, typeof result); // true "boolean"
