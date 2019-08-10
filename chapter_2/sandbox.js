64c83bb01f1ec880f21795b4bc981971c893c039
harjoth <harjoth.khara@gmail.com>
2019-08-10 16:26:11 -0700

    break and continue. the break keyword breaks us out of the loop. continue ignores the code block and continues with the next iteration

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,11 +1,27 @@
-// logical NOT (!)
+// break and continue - break takes you out of the loop if the condition preceding the break is met, there's no more iterations and its onto the next code block. continue jumps directly onto the next iteration without executing any code.
 
-let user = false;
+const scores = [50, 25, 0, 30, 100, 20, 10];
 
-//evaluation being changed not the variable value which will always be user = false.
-if(!user){
-console.log('you must be logged in to continue')
+for(let i = 0; i < scores.length; i++){
+
+if(scores[i] === 0){
+  continue;
+  //if this condition is met then its going to ignore the code block below and go onto the next iteration
 }
 
-console.log(!true);
-console.log(!false);
+  console.log('your score: ', scores[i]);
+
+  if(scores[i] === 100){
+    console.log('congrats, you got the top score!');
+    break;
+  }
+
+}
+
+// 0 is not logged to the console.
+
+// your score:  50
+// your score:  25
+// your score:  30
+// your score:  100
+// congrats, you got the top score!
