22218c581b1fbd936e85b268c3834be6d3842510
harjoth <harjoth.khara@gmail.com>
2019-08-10 16:09:44 -0700

    logical NOT ! - executes code if a condition is false. ! does the opposite of what its placed before i.e !(true) // false

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,11 +1,11 @@
-// logical operators - OR || AND &&
+// logical NOT (!)
 
-const password = 'p@ss12';
-  // both need to be true for this condition to be satisfied
-if(password.length >= 12 && password.includes('@')){
-  console.log('that password is mighty strong')
-} else if(password.length >= 8 || password.includes('@') && password.length >= 5){
-  console.log('that password is strong enough!');
-} else {
-  console.log('password is not strong enough');
+let user = false;
+
+//evaluation being changed not the variable value which will always be user = false.
+if(!user){
+console.log('you must be logged in to continue')
 }
+
+console.log(!true);
+console.log(!false);
