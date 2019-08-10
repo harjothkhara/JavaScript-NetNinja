ba7baa96ab66b3ef3ebc20393672a941321e98dd
harjoth <harjoth.khara@gmail.com>
2019-08-10 11:09:49 -0700

    loose vs strict comparison....// loose comparison (different types can still be equal); types= number, string...// strict comparison (different types cannot be equal);

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,37 +1,14 @@
-// Boolean - true/false - we use booleans to evaluate conditions in our code and check whether certain things are true or false.
-
-// booleans & comparisons
-console.log(true,false,"true","false");
-
-// methods can return booleans
-let email = 'luigi@thenetninja.co.uk';
-let names = ['mario', 'luigi', 'toad'];
-
-//let result = email.includes('@');
-//console.log(result); // true
-
-//let result = names.includes('bowser');
-//console.log(result); // false
-
-// comparison operators - compare 2 things together and return true or false
 let age = 25;
 
-// console.log(age == 25); // true
-// console.log(age == 30); // false
-// //is age not equal to 30?
-// console.log(age != 30); // true
-// console.log(age > 20); // true
-// console.log(age < 20); // false
-// console.log(age <= 25); // true
-// console.log(age >= 25); // true
+// loose comparison (different types can still be equal); types= number, string...
 
-let name = 'shaun';
+//console.log(age == 25); // true
+//console.log(age == '25'); // true
+//console.log(age != 25); // false
+//console.log(age != '25'); // false
 
-console.log(name == 'shaun'); // true
-console.log(name == 'Shaun'); // false
-// later letters in the alphabet are greater then earlier letters...looking at first letter of string
-console.log(name > 'crystal'); //  true -> s > c
-console.log(name > 'Shaun'); //  true -> lowercase letter is greater than an uppercase letter
-console.log(name > 'Crystal'); //  true -> s > c also, lowercase > uppercase
-
-// ==  abstract equality or loose equality, the values type is not considered on the comparison
+// strict comparison (different types cannot be equal);
+console.log(age === 25); // true
+console.log(age === '25'); // false - no type conversion going on now behind the scenes
+console.log(age !== 25);  // false
+console.log(age !== '25');  // true - age is not equal to the string 25, its equal to the number 25
