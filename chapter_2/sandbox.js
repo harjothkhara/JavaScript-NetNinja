210d5b0e016dce3fb2ca2020acdb03340d262508
harjoth <harjoth.khara@gmail.com>
2019-08-10 16:01:59 -0700

    Logical Operators to satisfy multiple condition checks OR || AND &&

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,11 +1,11 @@
-// else if statements - only one code block will run. we can chain as many else if statements as we want
+// logical operators - OR || AND &&
 
-const password = 'p@ass';
-
-if(password.length >= 12){
+const password = 'p@ss12';
+  // both need to be true for this condition to be satisfied
+if(password.length >= 12 && password.includes('@')){
   console.log('that password is mighty strong')
-} else if(password.length >= 8){
-  console.log('that password is long enough!');
+} else if(password.length >= 8 || password.includes('@') && password.length >= 5){
+  console.log('that password is strong enough!');
 } else {
-  console.log('password is not long enough');
+  console.log('password is not strong enough');
 }
