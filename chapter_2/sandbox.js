5e8cca1c2a1936a9480657b131fafea54356b375
harjoth <harjoth.khara@gmail.com>
2019-08-09 18:01:35 -0700

    Numbers - math operators +, -, *, /, **(to the power of), %(remainder)...order of operation - how you perform calculations when they become complex - B I D M A S Brackets Indexes(**) Division Multiplication Addition Subtraction.... likes = likes + 1; likes++; //same as adding 1....NaN - not a number trying to do some calculation that doesn't result in a number....concatinate numbers - take the numbers and adds it into the string

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,18 +1,43 @@
-//common string methods
+let radius = 10;
+const pi = 3.14;
 
-let email = 'mario@thenetninja.co.uk';
+//console.log(radius, pi);
 
-//lastIndexOf method finds the last index of a particular character('n') in a string
-// let result = email.lastIndexOf('n'); //14
+// math operators +, -, *, /, **(to the power of), %(remainder)-----------------------
 
-//slice method slices a section from the string, takes 2 args, 1 = starting point 2= ending point
-//let result = email.slice(2, 5); //rio
+//console.log(10/2);
+//let result = radius % 3; //takes the radius, divides it by 3 and gives us a remainder
+//1
 
-// substr() is very much like slice but the 2 args are different - 1.start position 2. characters you want to go along. starts at position 4 and gets 10 characters total afterwards
-// let result = email.substr(4,10) //o@thenetni
+//let result = pi * radius**2 //area of a circle = pi r squared
+//314
 
-// replace() method replaces a certain character in the string with another character. take 2 arguments 1st the character you want to find and the second the replacement character. it only replaces the first character it finds.
+// order of operation - how you perform calculations when they become complex - B I D M A S -----------
+// Brackets Indexes(**) Division Multiplication Addition Subtraction
 
-let result = email.replace('n', 'w') //mario@thewetninja.co.uk
+//let result = 5 * (10-3)**2
 
+//console.log(result);
+
+let likes = 10;
+
+// likes = likes + 1;
+
+// short hand notation:---------------
+// likes++; //same as adding 1
+//likes--; //same as subtracting 1
+
+//likes += 10; //take the current variable value and add 10 to it
+//likes -=5; // take the current variable value and subtract 5 from it
+//likes *=2; //take the current variable value and multiple 2 to it
+//likes /=2; //takes the current variable value and divide it by 2
+//console.log(likes);
+
+// NaN - not a number------------------ trying to do some calculation that doesn't result in a number
+//console.log(5 /'hello'); //NaN
+//console.log(5 * 'hello'); //NaN
+
+// concatinate numbers - take the numbers and adds it into the string
+
+let result = 'the blog has ' + likes + ' likes';
 console.log(result);
