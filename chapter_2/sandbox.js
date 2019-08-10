04221eff896e9402e3f15d24b3f25f11bb9ed978
harjoth <harjoth.khara@gmail.com>
2019-08-10 10:16:06 -0700

    arrays - store collection of data that relate to each other. to get to a specific item in the array -- console.log(ninjas[1]); arrays like strings, and numbers have properties and methods - join(), indexOf(), concat(), push(), pop()

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,24 +1,44 @@
-let radius = 10;
-const pi = 3.14;
-// template strings
-const title = 'Best reads of 2019';
-const author = 'Mario';
-const likes = 30;
+// storing string data inside the array
+let ninjas = ['shaun', 'ryu', 'chun-li'];
 
-// concatenation way - ok for 1 or 2 variables
-// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes ';
-// console.log(result);
+//ninjas[1] = 'ken'; // override certain values in different positions inside the array
+//console.log(ninjas[1]);
 
-// template string literal way (preferred) - job is to allow us to inject variables directly into the string without coming out of it and having to concatenate + signs like above.
-let result = `The blog called ${title} by ${author} has ${likes} likes`;
-console.log(result);
+// storing numbers inside the array
+//let ages = [20,25,30,35];
+//console.log(ages[2]);
 
+// can store different data types inside an array
+//let random = ['shaun', 'crystal', 30,20];
+//console.log(random)
 
-// creating html templates
-let html = `
-  <h2>${title}</h2>
-  <p>By ${author}</p>
-  <span>This blog has ${likes} likes</span>
-`;
+//arrays like strings, and numbers have properties and methods.
 
-console.log(html);
+// length property counts how many elements are inside the array
+//console.log(ninjas.length); //3
+
+// array methods ----------------
+
+// join() - takes an array and joins all the elements inside them into a single string
+//let result = ninjas.join(',')
+//console.log(result); //shaun,ryu,chun-li
+
+// indexOf() - finds the position numbers of an item inside the array
+//let result = ninjas.indexOf('chun-li');
+//console.log(result); //2
+
+// concat() -  take our array and it concatenates(joining) another array with it
+//let result = ninjas.concat(['ken', 'crystal'])
+//console.log(result); // ["shaun", "ryu", "chun-li", "ken", "crystal"]
+
+// push() - pushes on a new value onto an array and returns the length of the new array. the new length since we've just added a new item to the array. some methods don't alter the original value, while some methods do, the push method is one that alters the original value. for that reason this is a a destructive method. does it destroy it but changes it
+//let result = ninjas.push('ken');
+//console.log(result); //4
+//console.log(ninjas); // ["shaun", "ryu", "chun-li", "ken"]
+
+// pop() - another destructive method that changes the original value. pop takes of the end value. returns the value that it popped off from the array.
+let result = ninjas.push('ken');
+console.log(ninjas); // ["shaun", "ryu", "chun-li", "ken"]
+result = ninjas.pop(); // ken has been popped off from the end of the array
+console.log(ninjas); // ["shaun", "ryu", "chun-li"]
+console.log(result); //ken -- pop off value
