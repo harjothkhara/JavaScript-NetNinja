f8511b23a9b410066d462cb375df40e316944331
harjoth <harjoth.khara@gmail.com>
2019-08-11 16:15:40 -0700

    functions vs methods... same except the difference is the way we invoke a methods and how they are defined i.e methods are defined on an object or data type like a string where as a function are not.

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,64 +1,11 @@
-// regular function
+const name = 'shaun';
 
-// const calcArea = function(radius){
-//   return 3.14 * radius**2;
-// };
+// functions - block of code to do something that can be invoked and run at any point in our code. defined here and not on an object or data type.
+const greet = () => 'hello';
 
-// arrow function
+let resultOne = greet(); // invoking function directly. when we invoke this regular arrow function - function name + parenthesis
+console.log(resultOne); // hello
 
-// const calcArea = radius => {
-// return 3.14 * radius**2;
-// };
-
-// if we have more then one parameter
-// const calcArea = (radius, somethingelse) => {
-//   return 3.14 * radius**2;
-//   };
-
-// if we have no parameters - anonymous function
-// const calcArea = () => {
-//   return 3.14 * radius**2;
-//   };
-
-// if we only have a single return we can get rid of the return keyword
-// const calcArea = radius => 3.14 * radius**2;
-
-// binding of the 'this keyword
-
-// const area = calcArea(5)
-// console.log('area is:', area); // area is: 78.5
-
-// practice arrow function
-
-// regular function
-// const greet = function(){
-//   return 'hello, world';
-// }
-
-// arrow function
-//const greet = () => 'hello world'; // single line return so no need for return statement
-//const result = greet() //storing return value from function to result variable
-//console.log(result) // logging the value to the console
-
-// regular function
-// const bill = function(products, tax){
-//   let total = 0; // local variable
-//   for (let i = 0; i < products.length; i++){ // cycling through products array
-//     total += products[i] + products[i] * tax; //  cycling through products (array) and for each product we're getting the total and adding the product value plus any kind of tax
-//   }
-//   return total; // then in the end we're returning the total
-// }
-// logging this directly in the console instead of saving it to a variable like above. exactly the same.
- //console.log(bill([10, 15, 30], 0.2)) // 66
-
-// arrow function
-const bill = (products, tax) => {
-  let total = 0;
-  for (let i = 0; i < products.length; i++){
-    total += products[i] + products[i] * tax;
-  }
-  return total;
-};
-
- const result = bill([10, 15, 30], 0.2);
- console.log(result); //66
+// methods - invoked using dot notation. methods are functions but their functions associated with an object or data type like a string. methods are defined on an object or data type.
+let resultTwo = name.toUpperCase() // variable name + dot notation + method name. still a function.
+console.log(resultTwo); // SHAUN
