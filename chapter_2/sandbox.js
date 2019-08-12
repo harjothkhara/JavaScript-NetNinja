68fac54fe9ac9139eeb942f1e2262c77a761df93
harjoth <harjoth.khara@gmail.com>
2019-08-11 20:43:22 -0700

    adding methods - functions that are defined on an object. in order to use "this" inside a method  and in order to refer to the actual object the method is on so that you can access the properties on it  i.e user. blogs ...we need to use a regular function and not an arrow function  ...logsblogs(){ } //still regular function inside an object but a shorthand version...now we can use "this" inside it

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,4 +1,4 @@
-// object literals
+// object literals - adding methods
 
 //object
 let user = {
@@ -7,26 +7,43 @@ let user = {
   age: 30,
   email: 'crystal@thenetninja.co.uk',
   location: 'berlin',
-  blogs: ['why mac & cheese rules', '10 things to make with marmite']
+  blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+  //key value pair
+  //name of method and this is what it does
+  login: function(){
+    console.log('the user logged in');
+  },
+  logout: function(){ //still regular function
+    console.log('the user logged out');
+  },
+  logsblogs(){ //still regular function but a shorthand version
+    //console.log(this.blogs) //"this" refers to this user object
+    console.log('this user has written the following blogs');
+    this.blogs.forEach(blog => {
+      console.log(blog);
+    })
+  }
+  // logsblogs: () => {
+  //   console.log(this); // window object when an arrow function is used as a method
+  // }
 };
+//object name and then dot then the method name and we invoke it with parenthesis
+user.login();
 
-console.log(user);
+//string method - which is similar
+const name = 'mario';
+// method defined on a string object
+name.toUpperCase();
 
-// if we want to access one of the properties from this object we can use dot notation
-console.log(user.name); // crystal
+// a regular function isn't defined inside an object, a method is.
 
-// if you want to override one of the properties values in the user object you can use dot notation on the property value.
-user.age = 35;
-console.log(user.age); //35
+user.logout();
 
-// you can also access and update properties using square bracket notation
-console.log(user['name']); //crystal
-user['name'] = 'chun-li';
-console.log(user['name']); // chun-li
+// make a method to logout all the blogs - to do that we need to use the "this" keyword.
+user.logsblogs(); //this user has written the following blogs
+                 //why mac & cheese rules
 
-// occasionally square bracket notation can be useful if we're passing some kind of variable
-//passing this string via the variable into the square brackets
-const key = 'location';
-console.log(user[key]); // same as user['location]
+// "this" keyword is a context object and it represents the context that the current code is executed. depend on where we use it, its value is going to be different.
+console.log(this) //global context or window object
 
-console.log(typeof user); // object
+// when we use a normal function as a method and invoke that method JS sets the value of the "this" keyword to the object the method was used on. inside the function therefore we can use the "this" keyword to refer to the object itself. when we use an arrow function instead to create the method, JS now will not set the value of the "this" keyword to be the object when the method is a arrow function.
