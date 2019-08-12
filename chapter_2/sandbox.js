6437a2499f85f55386d43632914c874c04a3c587
harjoth <harjoth.khara@gmail.com>
2019-08-11 17:41:20 -0700

    callback functions in action - use of forEach(fancy loop) and a callback function to create a html template from array data.

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,44 +1,24 @@
-// callbacks & foreach
+// get a reference to the 'ul'
+const ul = document.querySelector('.people');
 
-// so far we've been passing in strings or numbers as arguments but we can also pass in a function as another argument, when we do this the function we pass in is called a callback function.
+const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
 
-//function expression called myFunc which is a arrow function. creating our own function here and passing in a callback function that we define below
-const myFunc = (callbackFunc) => {
-  // do something
-  let value = 50; // local variable
-  callbackFunc(value); // call callback function that was passed in
-};
+let html = ``;
 
-myFunc(function(value){ // myFunc() calling myFunc function but instead of passing in a string or something, instead, we're passing in a function as an argument
-  // do something
-  console.log(value);
-});
+people.forEach(function(person){
+  // create html template
+  console.log(person); // first parameter of the callback function returns back each element in the array, we've called the first argument person to represent this.
 
-// turning into an arrow function
-myFunc(value => {
-  // do something
-  console.log(value);
-});
+  // using template string literals to inject variables directly into our html template and then adding each iteration of the html template with each element to the html variable.
+  html += `<li style="color:purple">${person}</li>`
+})
 
-//example----------------------------
+// for each person we're adding the html template onto the html. each time we fire this callback function for each element, we're adding the html snippet onto the html.
 
-let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
-// forEach() method iterates over an array, a bit like a loop, but more elegant. expects a callback function as an argument
-// iterates through the people array, and for each element inside that array it calls this callback function, so its going to console.log('something') for each element in that array
-people.forEach(function(){
-  console.log('something')
-});
+console.log(html); // <li style="color:purple">mario</li><li style="color:purple">luigi</li><li style="color:purple">ryu</li><li style="color:purple">shaun</li><li style="color:purple">chun-li</li>
 
-// defining callback function somewhere else amd then passing it into the forEach(), or whatever method we're using.
-const logPerson = (person, index) => {
-  console.log(`${index} - hello ${person}`)
-}
 
-//inside this callback function we can take in some parameters, first is whatever the value is that we're iterating over, so every time we fire the function for each individual element, we get that element as our first parameter. I'm calling it person b/c it makes sense to use the singular of people (you can call it whatever you want). again, all we're doing is iterating through this array using foreach() and firing a callback function, which we pass through as an argument to the forEach() method for each individual item in the array and we get access to that person in the array each time around. we can also take in an optional parameter as an argument to the forEach() method, which is the index of the element. best to write callback functions directly in as an argument into the method, especially if its a small function, otherwise define the callback function somewhere else and pass it in - advisable for large functions.
-        // individual element,
-people.forEach((person, index) => {
-  console.log(index, person)
-});
+// now we want to output it to the browser 1.get a reference to an element on the page ---> const ul = document.querySelector('.people');
 
-// defining callback function somewhere else amd then passing it into the forEach(), or whatever method we're using. doing exactly the same thing as above.
-people.forEach(logPerson);
+// places the html inside the ul tag
+ul.innerHTML = html;
