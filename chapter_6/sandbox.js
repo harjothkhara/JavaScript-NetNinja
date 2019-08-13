c58bfcacf1e7394fd9ace8c2be87a99ab19b9574
harjoth <harjoth.khara@gmail.com>
2019-08-13 14:05:46 -0700

    adding and removing classes from elements exercise.. innerText gets us all the visible text inside of an element,  textContent - gets us all the text inside the tag regardless of whther its hidden or not. add(), remove(), and toggle()..3 methods we can use on this classList peroperty of elements to add, remove o toggle classes.

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -16,5 +16,27 @@ content.classList.remove('error');
 console.log(content.classList); //DOMTokenList [value: ""]
 
 // adding a new class
-content.classList.add('success');
-console.log(content.classList); // text should now be limegreen
+//content.classList.add('success');
+//console.log(content.classList); // text should now be limegreen
+
+//challenge: query all the elements and cycle through them and give the elements that have error inside the tag a error class, any p tag that has a class of success inside a success class.
+
+// get a reference to all the p tags
+const paras = document.querySelectorAll('p');
+console.log(paras);
+
+paras.forEach(p => {
+  if(p.textContent.includes('error')){
+    p.classList.add('error');
+  }
+  if(p.innerText.includes('success')){
+    p.classList.add('success');
+  }
+});
+
+// how to toggle classes
+const title = document.querySelector('.title');
+
+// if i use the toggle it will give it a class of test
+title.classList.toggle('test'); // toggle class added
+title.classList.toggle('test'); // toggle class removed
