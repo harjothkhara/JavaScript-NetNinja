d7fa0de996f620a993141a98225a73a0ff595f84
harjoth <harjoth.khara@gmail.com>
2019-08-15 11:31:06 -0700

    checking answers - JS step 1 - added an event listener to the form to listen for that submit event, then  prevented the default action of a page refresh, we've set a local variable score = 0 to begin with, then we've got the user answers from each input field,  we've cycled through those and for each answer we've also got the index (location in the array) and we've compared that answer with the equivalent  index in the correctAnswer array. if they're the same we add 25 to the score, meaning an extra 25%, if they're not the same we don't touch the score, and finally we log out the final value of the score at the end.

---
+++ ninja_quiz/app.js
@@ -0,0 +1,23 @@
+const correctAnswers = ['B', 'B', 'B','B'];
+const form = document.querySelector('.quiz-form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  let score = 0;       // A or B       // A or B      // A or B     // A or B
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+  // check answers
+
+  // index is the position of the array so we can compare with the correctAnswers array
+  userAnswers.forEach((answer, index) => {
+    // to check if this answer is equal to the equivalent position in the correctAnswers array, if they are equal(those 2 values) then we'll increase the score, if not we won't.
+    if(answer === correctAnswers[index]) {
+    // when we fire the first callback function, the answer will be form.q1.value and the index = 0, we're saying is this value e.g answer = A  at position 0 (what the user selected for q1) and correctAnswer = B at that same position of 0. in that case the code block would not fire because the user answer is incorrect. if they were equal then the code block would fire and add 25 points to the score.
+    score += 25; // adding 25 for correct answer
+    }
+  });
+
+  console.log(score); // 25 because the user selected only 1 correct answer, 50 because the user selected 2 correct answers...
+
+});
