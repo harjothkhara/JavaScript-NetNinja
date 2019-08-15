f194b8a6425a9fe2e7e6dc071245b9da2f48ba2f
harjoth <harjoth.khara@gmail.com>
2019-08-15 12:55:06 -0700

    intervals & animating the score - using the setInterval() method stored on the window object to increase our score.

--- ninja_quiz/app.js
+++ ninja_quiz/app.js
@@ -23,8 +23,17 @@ form.addEventListener('submit', e => {
 
   //show result on page
   scrollTo(0,0) // takes in an x and y coordinate
-  result.querySelector('span').textContent = `${score}%`; //updating text content inside the span
   result.classList.remove('d-none') //getting a class list of the div container and removing display none class.
+
+  let output = 0; //users score
+  const timer = setInterval(() => {
+    result.querySelector('span').textContent = `${output}%`; //updating text content inside the span
+    if(output === score){
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10);
 });
 
 // window object (global object) - all FE javascript objects come from the window object. we don't need to type out window because its automatically inferred b/c its on the global object.
@@ -41,3 +50,18 @@ form.addEventListener('submit', e => {
 // setTimeout(() => {
 //   alert('hello ninjas');
 // },3000);
+
+// callback function fires every 1 second over again
+// setInterval(() => {
+//   console.log('hello')
+// }, 1000);
+
+// stopping an interval
+// let i = 0;
+// const timer = setInterval(() => {
+//   console.log('hello')
+//   i++;
+//   if(i === 5){
+//     clearInterval(timer)
+//   }
+// }, 1000);
