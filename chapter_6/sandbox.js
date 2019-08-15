3fd310436e81f093909e42216ca2a96388750798
harjoth <harjoth.khara@gmail.com>
2019-08-14 20:05:04 -0700

    keyboard events - setAttribute() overrides the previous class.

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('.signup-form');
 const feedback = document.querySelector('.feedback');
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -7,7 +8,6 @@ form.addEventListener('submit', e => {
   // validation
   // gets us the value inside the input field
   const username = form.username.value;
-  const usernamePattern = /^[a-zA-Z]{6,12}$/;
 
   //test method returns a boolean
   if(usernamePattern.test(username)){
@@ -18,4 +18,17 @@ form.addEventListener('submit', e => {
     feedback.textContent = 'username must contain letters only & be between 6 & 12 characters long';
   }
 
+});
+
+// live feedback
+form.username.addEventListener('keyup', e => {
+  //console.log(e); inside the event object we get info on which keys are being fired by the user
+  //console.log(e.target.value);
+  if(usernamePattern.test(e.target.value)){
+    form.username.setAttribute('class', 'success')
+    //console.log('passed');
+  } else {
+    //console.log('failed');
+    form.username.setAttribute('class', 'error')
+  }
 });
