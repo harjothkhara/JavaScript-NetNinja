4442752a6ba67e2ba9d46f340aa7c559e7cb5fce
harjoth <harjoth.khara@gmail.com>
2019-08-15 18:17:50 -0700

    map method - takes an array and maps that into a new array. it iterates an array and returns an updated value for each item in the array and then it pushes that updated value into a new array. this is a non destructive method in that it doesn't directly change the original array.

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -1,20 +1,22 @@
-const scores = [10,30,15,25,50,40,5];
+const prices = [20,10,30,25,15,40,80,5];
 
-// const filteredScores = scores.filter(score => {
-//   return score >20;
-// })
+//const salePrice = prices.map(price => price / 2);
+//console.log(salePrice); // [10, 5, 15, 12.5, 7.5, 20, 40, 2.5]
 
-// console.log(filteredScores);
-
-const users = [
-  {name: 'shaun', premium: true},
-  {name: 'yoshi', premium: false},
-  {name: 'mario', premium: false},
-  {name: 'chun-li', premium: true}
+const products = [
+  {name: 'gold star', price: 20},
+  {name: 'mushroom', price: 40},
+  {name: 'green shells', price: 30},
+  {name: 'banana skin', price: 10},
+  {name: 'red shells', price: 50},
 ];
 
-const premiumUsers = users.filter(user =>
-  user.premium; // if its true its going to keep it in the array, if false it will kick it out.
-)
+const saleProducts = products.map(product => {
+  if(product.price > 30){
+    return {name: product.name, price: product.price / 2}
+  } else {
+    return product
+  }
+});
 
-console.log(premiumUsers);
+console.log(saleProducts, products); // original array is still the same. we have not directly changed items in the original array like we would have done here: i.e product.price = product.price /2
