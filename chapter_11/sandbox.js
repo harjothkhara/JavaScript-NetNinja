763417382c7bbc4e3272872fd70a13fc63c03036
harjoth <harjoth.khara@gmail.com>
2019-08-18 17:27:34 -0700

    Building a Digital Clockl

--- chapter_11/sandbox.js
+++ chapter_11/sandbox.js
@@ -1,31 +1,25 @@
-// timestamps
-const before = new Date('March 18 2019 3:42:59'); // creating a date sometime in the past, just one date string format. could be stored in a database
-const now = new Date(); // get a new date object using the date constructor
-//console.log(now.getTime(), before.getTime()); // if we wanted to get the timestamp from that date we would use the getTime() method. // 1566167673219 - number of milliseconds since the Jan 1, 1970.
-// 1566168250412 Sun Aug 18 2019 03:42:59 GMT-0700 (Pacific Daylight Time)
-// 1566168302884(now) 1566124979000(before)
+// reference where we will display our clock
+const clock = document.querySelector('.clock');
+// function that will update the time on our digital clock every second
+const tick = () => {
 
-// if we wanted to compare two dates together we could compare two timestamps together and figure out the time between them, the difference in milliseconds.
+  const now = new Date();
 
-const diff = now.getTime() - before.getTime();
-console.log(diff); // 13263020942 -> now we can convert to minutes, hours, days...
-// maybe we want to see that this blog was created 20 days ago depending on the date...
+  const h = now.getHours(); // grabbing hour from the new Date() object
+  const m = now.getMinutes();
+  const s = now.getSeconds();
 
-// difference between timestamps in minutes -
-//1000 miliseconds in a second and 60 seconds in a minute
-const mins = Math.round(diff / 1000 / 60); // 221050
-const hours = Math.round(mins / 60);
-const days = Math.round(hours / 24);
+  //console.log(h,m,s); --> // now lets output this to the DOM/browser
+  // we need to create some html template and inject it into the clock div.
 
-console.log(mins, hours, days); //221052 3684 154
+  const html = `
+    <span>${h}</span> :
+    <span>${m}</span> :
+    <span>${s}</span>
+  `;
 
-console.log(`the blog was written ${days} days ago`); // the blog was written 154 days ago
-
-// converting timestamps into date objects
-const timestamp = 1675938474990; //between a certain date and Jan 1, 1970 ...e.g now.getTime()
-// need to convert into a date object
-console.log(new Date(timestamp)) // converts timestamp into the date object
-//1675938474990 ---> Thu Feb 09 2023 02:27:54 GMT-0800 (Pacific Standard Time)
-
-const Day = new Date(timestamp)
-console.log('getDay:', Day.getDay()); // 4 -> Thursday
+  clock.innerHTML = html;
+// every second we're updating the html, we're overriding it with the new template which is going to contain the new hours, minutes and seconds, every second!
+};
+// 1000 miliseconds = 1 sec
+setInterval(tick, 1000); //console logging the hour every second. calling the tick function every second. now we're logging the minutes in the hour every second. now we're logging the seconds of the minute of the hour every second.
