b61cd1050731632be8f8842929704e333134b27c
harjoth <harjoth.khara@gmail.com>
2019-09-08 20:44:07 -0700

    throwing & catching errors

--- chapter_12 - async/sandbox.js
+++ chapter_12 - async/sandbox.js
@@ -1,33 +1,17 @@
 // async & await
 
 const getTodos = async () => {
-  const response = await fetch('todos/luigi.json'); // await stops it from assigning a value to the variable until the promise is resolved. once we get the data back we have to use the json method to get that data back.
-  // console.log(response);
+  const response = await fetch('todos/luigis.json');
+
+  if (response.status !== 200) {
+    throw new Error('cannot fetch the data');
+  }
+
   const data = await response.json();
-  // console.log(data);
+
   return data;
 };
-// non blocking
-console.log(1);
-console.log(2);
 
-getTodos().then(data => {
-  console.log('resolved', data);
-});
-console.log(3);
-console.log(4);
-
-// const test = getTodos();
-// console.log(test); // returns a promise
-
-// fetch('todos/luigi.json')
-//   .then(response => {
-//     console.log('resolved', response);
-//     return response.json(); //returns a promise
-//   })
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(err => {
-//     console.log('rejected', err);
-//   });
+getTodos()
+  .then(data => console.log('resolved', data))
+  .catch(err => console.log('error', err.message));
